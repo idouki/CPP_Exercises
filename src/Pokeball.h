9ade51b98815180657162f8f32f85636b7315106
DOUKI IDIR <idir.douki@heckel02.u-pem.fr>
2023-02-09 15:19:55 +0100

    Test 03

--- src/Pokeball.h
+++ src/Pokeball.h
@@ -9,21 +9,16 @@ public:
         return _empty;
     }
 
-    void store(const Pokemon pokemon){
-        _pokemon = pokemon;
+    void store(PokemonPtr pokemon){
+        _pokemon = std::move(pokemon);
         _empty = false;
     }
 
-    void store(const PokemonPtr pokemon){
-        _pokemon = *pokemon;
-        _empty = false;
-    }
-
-    Pokemon pokemon() const{
-        return _pokemon;
+    Pokemon& pokemon() const{
+        return *_pokemon;
     }
 
 private:
     bool _empty = true;
-    Pokemon _pokemon;
+    PokemonPtr _pokemon;
 };
