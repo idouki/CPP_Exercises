8ef38f18bc980992c36fda9178282a01499e78a8
DOUKI IDIR <idir.douki@heckel02.u-pem.fr>
2023-02-09 15:42:58 +0100

    Test 05

--- src/PC.h
+++ src/PC.h
@@ -1,6 +1,27 @@
 #pragma once
 
+#include "Pokemon.h"
+#include "vector"
+
 // A PC is the place where Pokemons get automagically sent when the trainer's pockets are full.
 // When a Pokemon is transferred to the PC, this one takes ownership.
 class PC
-{};
+{
+public:
+    bool empty() const {
+        return _pokemons.empty();
+    }
+
+    std::vector<PokemonPtr>& pokemons() { // TODO : pourquoi ça ne marche pas avec const
+        return _pokemons;
+    }
+
+    void transfer(PokemonPtr pokemon){
+        if(pokemon != nullptr){
+            _pokemons.push_back(std::move(pokemon));
+        }
+    }
+
+private:
+    std::vector<PokemonPtr> _pokemons;
+};
