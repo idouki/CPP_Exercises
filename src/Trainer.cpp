1b1af7538741fd55d40176ccf9b7bc7bbba35616
idir.douki <doukiidir17@gmail.com>
2023-02-14 22:32:43 +0100

    Test 8

--- src/Trainer.cpp
+++ src/Trainer.cpp
@@ -1,2 +1,20 @@
 #include "Trainer.h"
 
+void Trainer::capture(PokemonPtr pokemon)
+{
+    if (pokemon == nullptr)
+    {
+        return;
+    }
+
+    for (auto &pokeball : _pokeballs)
+    {
+        if (pokeball.empty())
+        {
+            pokeball.store(std::move(pokemon));
+            return;
+        }
+    }
+
+    _pc.transfer(std::move(pokemon));
+}
--- src/Trainer.h
+++ src/Trainer.h
@@ -2,34 +2,26 @@
 
 #include "PC.h"
 #include "Pokeball.h"
+#include <array>
 
 // A person that captures Pokemons and makes them fight.
 class Trainer
 {
 public:
-
-    Trainer(const std::string name, PC& pc):
-        _name {name},
-        _pc {pc}
-    {}
-
-    const std::string& name() const { return _name; }
-
-    const std::array<Pokeball, 6>& pokeballs() const { return _pokeballs; } // TODO Pourquoi, il faut mettre const
-
-    void capture(PokemonPtr pokemon){
-        if(pokemonCount == 5 || pokemon == nullptr){
-            return;
-        }
-        auto& pokeball = _pokeballs[pokemonCount];
-        pokeball.store(pokemon);
-        pokemonCount++;
+    Trainer(const std::string name, PC &pc) : _name{name},
+                                              _pc{pc}
+    {
     }
+
+    const std::string &name() const { return _name; }
+
+    const std::array<Pokeball, 6> &pokeballs() const { return _pokeballs; } // TODO Pourquoi, il faut mettre const
+
+    void capture(PokemonPtr pokemon);
 
 private:
     const std::string _name;
-    PC& _pc;
+    PC &_pc;
     std::array<Pokeball, 6> _pokeballs;
-    int pokemonCount  = 0;
-    
+    int pokemonCount = 0;
 };
