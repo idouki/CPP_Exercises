fd8811d230dd444f9d023ea2233734d46fbb38e1
DOUKI IDIR <idir.douki@heckel02.u-pem.fr>
2023-03-29 12:30:18 +0200

    Test 01 & Test 03

--- src/image.hpp
+++ src/image.hpp
@@ -11,3 +11,54 @@
 #include <array>
 #include <iostream>
 
+template <typename P, size_t W, size_t H>
+class Image{
+public:
+    Image() = default;
+
+    Image(const P& pixel):
+        _pixels{}{
+            fill_array(pixel);
+        }
+
+    Image<P, W, H>(const std::function<P(const size_t i, const size_t j)> & functor)
+    {
+        for(size_t j = 0; j < H; j++)
+            for(size_t i = 0; i < W; i++)
+                _pixels[j][i] = functor(i, j);
+
+    }
+
+    P& operator()(const size_t i, const size_t j)
+    {
+        return _pixels[j][i];
+    }    
+
+    const P& operator()(const size_t i, const size_t j) const
+    {
+        return _pixels[j][i];
+    }    
+
+private:
+    void fill_array(const P &pixel){
+        for(size_t i = 0; i < H; i++){
+            std::array<P, W> tmp;
+            tmp.fill(pixel);
+            _pixels[i] = tmp;
+        }
+    }
+
+private:    
+    std::array<std::array<P, W>, H> _pixels;
+};
+
+template <typename P, size_t W, size_t H>
+const Image<P, W, H> operator+(const Image<P, W, H> &l, const Image<P, W, H> &r) {
+    Image<P, W, H> image = {};
+
+    for(size_t j = 0; j < H; j++)
+        for(size_t i = 0; i < W; i++)
+            image(i, j) = l(i, j) + r(i, j);
+        
+    return image;
+}
--- src/pixels.hpp
+++ src/pixels.hpp
@@ -4,3 +4,39 @@
 
 #include <stdint.h>
 
+struct RGB{
+    uint8_t r,g,b;
+};
+
+struct RGBA: RGB{
+    uint8_t a;    
+};
+
+struct Luma{
+    uint8_t gray;    
+};
+
+template<typename A, typename B>
+A from(const B & stuff);
+
+template<typename A>
+A from(const A & x)
+{
+    return x;
+}
+
+template <>
+RGB from(const RGBA& r){
+    return  {r.r, r.g, r.b};
+}
+
+template <>
+RGBA from(const RGB& r){
+    return  {r.r, r.g, r.b, 255};
+}
+
+template <>
+Luma from(const RGB& r){
+    return {lib::rgb_to_grayscale(r.r, r.g, r.b)};
+}
+
